8733bd9c7ce4d2a713631e66433474406078c6c1
Illia Kryvorotenko <ilakrivorotenko@gmail.com>
2026-01-28 20:34:06 +0100

    implemented personalities module

--- src/personalities/personalities.module.ts
+++ src/personalities/personalities.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PersonalitiesService } from './personalities.service';
 import { PersonalitiesController } from './personalities.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Personality } from './entities/personality.entity';
+import { RagDocumentEntity } from '../rag-documents/entities/rag-document.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Personality, RagDocumentEntity])],
   controllers: [PersonalitiesController],
   providers: [PersonalitiesService],
 })
